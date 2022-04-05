a879e43b12e28e3daacc3cd027c806db8500ca99
Michał Pamięta <michalpamieta1@gmail.com>
2022-04-05 02:12:24 +0200

    Zadanie 03 One Page +RWD
    W ramach zadania należy Przebudować stronę zgodnie z ideą Single PageApplication i RWD (ewentualnie stworzyć nową stronę w ramach zadania).
    
    Strona ma być w pełni Responsywna, tzn obsługa rozdzielczości dla:
    - Desktopów
    - Tabletów (od 640)
    - Telefonów (od 480)
    
    Oprócz tego wymagana jest:
     - Zgodność (W3C dla HTML5 i CSS3)
     - GTMetrix – co najmniej jasnozielony dla wszystkich elementów strony (ignorujemy parametry dotyczące serwera)
    
    Strona powinna posiadać bardziej rozbudowany layout niż w przypadku pierwszego zadania, zawierający elementy wymagające wykorzystania responsywności (np. kilka kolumn tekstu, menu przełączane w zależności od urządzenia, obrazki, etc.)
    
    Tak jak w przypadku poprzednich prac domowych, strona musi być dostępna online i w ramach zadania przesyłamy link do niej.

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+const app = {
+    pages: [],
+    show: new Event('show'),
+    init: function(){
+        document.querySelectorAll('.navbar-link').forEach((link)=>{
+            link.addEventListener('click', app.nav);
+        })
+        history.replaceState({}, 'Home', '#home');
+        window.addEventListener('popstate', app.poppin);
+    },
+    nav: function(ev){
+        ev.preventDefault();
+        let currentPage = ev.target.getAttribute('data-target');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(currentPage).classList.add('active');
+        history.pushState({}, currentPage, `#${currentPage}`);
+        document.getElementById(currentPage).dispatchEvent(app.show);
+    },
+    poppin: function(ev){
+        let hash = location.hash.replace('#' ,'');
+        document.querySelector('.active').classList.remove('active');
+        document.getElementById(hash).classList.add('active');
+        history.pushState({}, currentPage, `${currentPage}`);
+        document.getElementById(hash).dispatchEvent(app.show);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', app.init);
